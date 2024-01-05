97bef0ea265d0532c7a994f5e44b5ce5dedcf315
Franceschio <coppolaciccio1@gmail.com>
2024-01-06 00:30:56 +0100

    Tetris-basic V1.1

--- tetris.js
+++ tetris.js
@@ -719,13 +719,14 @@ const setPauseMenu = () => {
 };
 
 const touchEvents = (e) => {
+  newX ? (lastTouchX = newX) : null;
   let isTouchingBorder = false; //Una condizione di uscita per evitare per le if
   if (!document.querySelector(".mainMenu")) {
     e.preventDefault();
     let touch = e.touches[0]; // Ottieni il primo tocco
-    newX = Math.floor((touch.pageX - gameBoard.offsetLeft) / 25); // Sposta il tetromino sull'asse X
-    newY = Math.floor((touch.pageY - gameBoard.offsetTop) / 25); // Sposta il tetromino sull'asse Y
-    if (newX > lastTouchX + 0.5) {
+    newX = Math.floor((touch.pageX - gameBoard.clientWidth) / 25) * 10; // Sposta il tetromino sull'asse X
+    newY = Math.floor((touch.pageY - gameBoard.clientHeight) / 25); // Sposta il tetromino sull'asse Y
+    if (newX > lastTouchX) {
       clearTouchY();
       if (
         currenTetr.some(
@@ -739,7 +740,7 @@ const touchEvents = (e) => {
         moveRight();
       }
       touchMoved = true;
-    } else if (newX < lastTouchX - 0.5) {
+    } else if (newX < lastTouchX) {
       clearTouchY();
       if (
         currenTetr.some((i) => (tetrPosition + i) % width === 0) ||
